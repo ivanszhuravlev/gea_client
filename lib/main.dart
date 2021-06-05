d2787a7c443d1e160a2358aa7ab37e985441ac9c
ivanszhuravlev <ivanszhuravlev@gmail.com>
2021-06-05 09:41:25 +0300

    Add sidebar's navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:gea/models/app_model.dart';
 import 'package:gea/navigator/root_navigator.dart';
 import 'package:provider/provider.dart';
-import 'modules/sidebar.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,33 +14,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider<AppModel>(create: (context) => AppModel())
       ],
-      child: MaterialApp(
-        title: 'GEA',
-        theme: ThemeData(
-          pageTransitionsTheme: PageTransitionsTheme(builders: {
-            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          }),
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Дашборд"),
-          ),
-          body: Row(
-            children: [
-              Sidebar(),
-              Padding(
-                padding: EdgeInsets.only(left: 0.0, top: 12.0, right: 0.0, bottom: 12.0),
-                child: VerticalDivider(),
-              ),
-              Expanded(
-                child: RootNavigator(),
-              ),
-            ],
-          ),
-          backgroundColor: Colors.white,
-        ),
-      ),
+      child: RootNavigator()
     );
   }
 }
--- lib/models/app_model.dart
+++ lib/models/app_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:gea/api/application.dart';
 import 'package:gea/types/application.dart';
+import 'package:collection/collection.dart';
 
 class AppModel extends ChangeNotifier {
   final List<ApplicationInfo> _apps = [];
@@ -22,5 +23,9 @@ class AppModel extends ChangeNotifier {
     var list = await client.list();
     _apps.addAll(list);
     notifyListeners();
+  }
+
+  ApplicationInfo? item({Key? key, String id = ""}) {
+    return _apps.firstWhereOrNull((element) => element.id == id, );
   }
 }
